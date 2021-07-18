5b5f600f589e1661b7703d27f159b5a269370944
VIDIT VISHAL <76274089+viditvishal@users.noreply.github.com>
2021-07-19 02:39:03 +0530

    Add files via upload

---
+++ C program to print ASCII Value.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+#include <stdio.h>
+int main() {  
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);  
+    
+    // %d displays the integer value of a character
+    // %c displays the actual character
+    printf("ASCII value of %c = %d", c, c);
+    
+    return 0;
+}
